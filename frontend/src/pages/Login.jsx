f256ca7f962cca4a7dcf89917f5af93b7e912ea2
Alexandru Stanica <stanica.alexandru99@gmail.com>
2022-03-22 22:13:32 +0200

    Added UI for Login and Register forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,63 @@
+import { useState, useEffect } from "react";
+
 function Login() {
-    return <div>Login</div>;
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+    };
+
+    return (
+        <>
+            <section className="heading">
+                <h2>Sign in</h2>
+            </section>
+
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-block">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
 }
 
 export default Login;
